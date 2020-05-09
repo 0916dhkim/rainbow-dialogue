af1eec068f37630f48d162f3751adbc252786937
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-09 16:35:53 -0400

    Implement Input Parsing

---
+++ src/parse_input.c
@@ -0,0 +1,46 @@
+#include <parse_input.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char *get_next_dialog(char *s, int maxlinelength, int maxlinecount) {
+    // Start with an empty string.
+    char *cur = s;
+    *cur = '\0';
+
+    int end_of_dialog = 0; // End of dialog reached. (newline or EOF)
+    int end_of_file = 0; // EOF reached.
+    int linecount;
+    for (linecount = 0; linecount < maxlinecount; linecount++) {
+        int linelength;
+        for (linelength = 0; linelength < maxlinelength; linelength++) {
+            // Read 1 char.
+            char c = (char) fgetc(stdin);
+            // Handle newline and EOF
+            if (c == '\n') {
+                end_of_dialog = 1;
+                end_of_file = 0;
+                break;
+            }
+            if (c == EOF){
+                end_of_dialog = 1;
+                end_of_file = 1;
+                break;
+            }
+            // Concat 1 char.
+            *(cur++) = c;
+        }
+
+        if (end_of_dialog) {
+            break;
+        }
+        // Wrap dialog line.
+        *(cur++) = '\n';
+    }
+
+    if (s == cur && end_of_file) {
+        // Could not read string
+        // because EOF is reached.
+        return NULL;
+    }
+    return s;
+}
---
+++ src/parse_input.h
@@ -0,0 +1,6 @@
+// Read one line from stdin
+// and wrap the string.
+// Return s, or NULL if EOF.
+// maxlinelength = limit where string gets wrapped (not counting newline)
+// maxlinecount = number of lines allowed in one dialog
+char *get_next_dialog(char *s, int maxlinelength, int maxlinecount);
