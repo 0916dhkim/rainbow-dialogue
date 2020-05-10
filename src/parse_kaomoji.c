bc78761bfc213e69c7237c732ab8fd153c0db9d9
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-09 21:03:43 -0400

    Implement Kaomoji File Parser

---
+++ src/parse_kaomoji.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int parse_kaomoji(char **target, char *filename, int max_kaomoji_count, int max_kaomoji_length) {
+    int kaomoji_count = 0;
+    int line_position = 0;
+    FILE *kaomoji_file = fopen(filename, "r");
+    char c;
+    while((c = fgetc(kaomoji_file)) != EOF) {
+        if (c == '\n') {
+            // Ignore newline.
+            target[kaomoji_count++][line_position] = '\0';
+            continue;
+        }
+        if (target[kaomoji_count] == NULL) {
+            target[kaomoji_count] = calloc(max_kaomoji_count, sizeof(char));
+        }
+        target[kaomoji_count][line_position++] = c;
+    }
+    fclose(kaomoji_file);
+    return kaomoji_count;
+}
---
+++ src/parse_kaomoji.h
@@ -0,0 +1,3 @@
+// Parse kaomoji file and populate target kaomoji array.
+// Return number of successfully parsed kaomojis.
+int parse_kaomoji(char **target, char *filename, int max_kaomoji_count, int max_kaomoji_length);
